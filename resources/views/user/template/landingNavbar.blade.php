6ecdce9e7b961967688e1eda1bad16bb880ec4b9
Arima1213 <arima3992@gmail.com>
2024-08-12 12:22:40 +0700

    chore: Add responsive mobile navigation toggle button to landingNavbar.blade.php

--- resources/views/user/template/landingNavbar.blade.php
+++ resources/views/user/template/landingNavbar.blade.php
@@ -66,6 +66,15 @@
 						</a>
 					</li>
 				@endif
+				<li class="nav-item d-xl-none d-flex align-items-center ps-3">
+					<a href="javascript:;" class="nav-link text-body p-0" id="iconNavbarSidenav">
+						<div class="sidenav-toggler-inner">
+							<i class="sidenav-toggler-line"></i>
+							<i class="sidenav-toggler-line"></i>
+							<i class="sidenav-toggler-line"></i>
+						</div>
+					</a>
+				</li>
 			</ul>
 		</div>
 	</div>
