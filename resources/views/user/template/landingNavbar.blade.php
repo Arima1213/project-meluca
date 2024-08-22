22841b1d9ef487468117ffffaa3747e8a3ecc73f
Arima1213 <arima3992@gmail.com>
2024-08-23 02:30:06 +0700

    chore: Refactor landingNavbar.blade.php for improved user experience and consistency

--- resources/views/user/template/landingNavbar.blade.php
+++ resources/views/user/template/landingNavbar.blade.php
@@ -7,16 +7,15 @@
 			<ul class="navbar-nav justify-content-end">
 				@if (auth()->check())
 					<li class="nav-item d-flex align-items-center">
-						<a href="javascript:;" class="nav-link text-body font-weight-bold px-0">
-							<i class="fa fa-user me-sm-1"></i>
-							<span class="d-sm-inline d-none"><strong>{{ auth()->user()->email }}</strong></span>
+						<a href="{{ route('cart.index') }}" class="nav-link text-body font-weight-bold position-relative px-0">
+							<i class="fa fa-shopping-cart me-sm-1"></i>
+							<span class="badge bg-danger position-absolute start-100 translate-middle badge-rounded-pill top-0" id="cartItemCount"></span>
 						</a>
 					</li>
 					<li class="nav-item d-flex align-items-center">
-						<a href="{{ route('cart.index') }}" class="nav-link text-body font-weight-bold position-relative px-0">
-							<i class="fa fa-shopping-cart me-sm-1"></i>
-							<span class="d-sm-inline d-none">Cart</span>
-							<span class="badge bg-danger position-absolute start-100 translate-middle badge-rounded-pill top-0" id="cartItemCount"></span>
+						<a href="javascript:;" class="nav-link text-body font-weight-bold px-0">
+							<i class="fa fa-user me-sm-1"></i>
+							<span class="d-sm-inline d-none"><strong>{{ auth()->user()->email }}</strong></span>
 						</a>
 					</li>
 				@else
@@ -51,7 +50,11 @@
 			.then(response => response.json())
 			.then(data => {
 				const cartItemCount = document.getElementById('cartItemCount');
-				cartItemCount.textContent = data.count;
+				if (data.count > 0) {
+					cartItemCount.textContent = data.count;
+				} else {
+					cartItemCount.style.display = 'none';
+				}
 			})
 			.catch(error => console.error('Error fetching cart item count:', error));
 	}
